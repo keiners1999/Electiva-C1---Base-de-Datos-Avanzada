57c829d0c85870351306f3b1dbd9453a4f53ebd6
Rafael Pinto <100289672+RpintoM@users.noreply.github.com>
2022-03-03 21:31:21 -0500

    Add files via upload

---
+++ Actividad 1.py
@@ -0,0 +1,9 @@
+
+#base = float(input("por favor ingrese la base de su triangulo ==> "))
+#altura = float(input("por favor ingrese la altura de su triangulo ==> "))
+
+#area = (base*altura)/2
+
+#input(f"El area de su triangulo es==> {area:.0f} ")
+
+
