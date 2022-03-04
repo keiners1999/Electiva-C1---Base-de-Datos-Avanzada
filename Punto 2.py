102fc77979b797710d210c2efc31a4534e7f2d11
John Doe <kbarraza@uniguajira.edu.co>
2022-03-03 23:44:13 -0500

    Punto 2 sin Return

--- Punto 2.py
+++ Punto 2.py
@@ -1,12 +1,7 @@
-
-a = float(input("por favor darle valor 'a' ==> "))
-b = float(input("por favor darle valor 'b' ==> "))
-c = float(input("por favor darle valor 'c' ==> "))
-d = (b**2)-4*a*c
-
-if d < 0:
-    print("el resultado es indefinido")
-else:
-    print(f"el resultado de su ecuacion - es: {(-b-(d)**1/2)/(2*a)}")
-    print(f"el resultado de su ecuacion + es: {(-b+(d)**1/2)/(2*a)}")
+def SinReturn():
+    a = float(input("por favor darle valor 'a' ==> "))
+    b = float(input("por favor darle valor 'b' ==> "))
+    
+    print(f"el resultado de su ecuacion es: {pow((a**2)+(b**2),1/3)}")
+print(SinReturn())
 
